e3f5525bb0bb0b2a8da254e2a3e9f40afe9a2ca6
Hubert Figuiere <hub@figuiere.net>
2007-09-19 16:07:00 +0000

    More discover, more screenshots

--- discover/index.php
+++ discover/index.php
@@ -3,16 +3,53 @@ require("../template.php");
 
 print_page("Go-OO! - Discover", "discover",
    "Discover OpenOffice",
+
+   "<p>Here you'll discover what Go-oo has to offer in addition to the ".
+   "regular build of OpenOffice.org</p>\n" .
+
    "<h4>GStreamer integration</h4>".
-   "<p>go-oo.org supports multimedia content into your document using the ".
+   "<p>Go-oo supports multimedia content into your document using the ".
    "gstreamer multimedia framework".
    "</p>".
-   "<p><b>Before</b><br /><a href=\"oo-gstreamer.png\"><img src=\"oo-gstreamer-thumb.png\" /></a>\n".
-   "<p><b>After</b><br /><a href=\"go-gstreamer.png\"><img src=\"go-gstreamer-thumb.png\" /></a>\n".
-   "</p>".
+   "<table>".
+   "<tr><th><b>Before</b></th>".
+   "<th><b>After</b></th></tr>".
+   "<tr><td><a href=\"oo-gstreamer.png\"><img src=\"oo-gstreamer-thumb.png\" /></a></td>\n".
+   "<td><a href=\"go-gstreamer.png\"><img src=\"go-gstreamer-thumb.png\"".
+   "/></a></td></tr>\n".
+   "</table>".
+
    "<h4>VBA support</h4>".
-   "<p>go-oo.org provide VBA support for OpenOffice.org</p>".
-   "<p><a href=\"go-excel-vba.png\"><img src=\"go-excel-vba-thumb.png\" /></a></p>"
+   "<p>Go-oo provide VBA support for OpenOffice.org</p>".
+   "<p><a href=\"go-excel-vba.png\">".
+   "<img src=\"go-excel-vba-thumb.png\" /></a></p>".
+
+   "<h4>Excel&trade; Pivot Table</h4>".
+   "<p>Go-oo support Excel Pivot Table</p>".
+   "<p><a href=\"go-xls-pivot-table.png\">".
+   "<img src=\"go-xls-pivot-table-thumb.png\" /></a></p>".
+
+   "<h4>WordPerfect Graphics import</h4>".
+   "<p>Go-oo imports graphics in the WPG format coming from WordPerfect".
+   "which supplement the WordPerfect importer also available in Go-oo.</p>".
+   "<a href=\"go-wpg-support.png\"><img src=\"go-wpg-support-thumb.png\" /></a>".
+
+   "<h4>MS-Works import</h4>".
+   "<p>Go-oo supports MS-Works files.</p>".
+   "<a href=\"go-msworks.png\"><img src=\"go-msworks-thumb.png\" /></a>".
+
+   "<h4>Office OpenXML import</h4>".
+   "<p>Go-oo integrates the work in progress made with the Microsoft ".
+   "Office OpenXML format that are used by Microsoft Office 2007.</p>".
+   "<table>" .
+   "<tr>".
+   "<th>Word</th><th>Excel</th><th>Excel</th></tr>".
+   "<tr><td><a href=\"go-docx-sample.png\"><img src=\"go-docx-sample-thumb.png\"/></a></td>".
+   "<td><a href=\"go-xslx-sample1.png\"><img src=\"go-xslx-sample1-thumb.png\"/></a></td>".
+   "<td><a href=\"go-xslx-sample2.png\"><img src=\"go-xslx-sample2-thumb.png\"/></a></td></tr>".
+   
+
+   ""
    );
 
 ?>
