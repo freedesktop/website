cca60c5729d2259873f7a2eed8c4fd187c380f63
Hubert Figuiere <hub@figuiere.net>
2007-09-19 09:24:58 +0000

    Added screenshots

--- discover/index.php
+++ discover/index.php
@@ -2,9 +2,17 @@
 require("../template.php");
 
 print_page("Go-OO! - Discover",
-   "Discover",
-   "<p>Content".
-   "</p>");
+   "Discover OpenOffice",
+   "<h4>GStreamer integration</h4>".
+   "<p>go-oo.org supports multimedia content into your document using the ".
+   "gstreamer multimedia framework".
+   "</p>".
+   "<p><a href=\"go-gstreamer.png\"><img src=\"go-gstreamer-thumb.png\" /></a>".
+   "</p>".
+   "<h4>VBA support</h4>".
+   "<p>go-oo.org provide VBA support for OpenOffice.org</p>".
+   "<p><a href=\"go-excel-vba.png\"><img src=\"go-excel-vba-thumb.png\" /></a></p>"
+   );
 
 ?>
 
