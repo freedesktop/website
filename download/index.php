a33b9320db28d57273287023a6fedad1e5053e14
Hubert Figuiere <hub@figuiere.net>
2007-09-19 14:43:56 +0000

    Update download section

--- download/index.php
+++ download/index.php
@@ -5,33 +5,51 @@ print_page("Go-OO! - Download", "download",
 		   "Download OpenOffice.org",
                    "Choose the version of OpenOffice that matches your computer's operating system.".
                    "<dl>".
-                   "<dt><a href=\"http://go-oo.org/tstnvl/GoOo/\">Windows</a> - simply download the zip file and run the".
-                   "embedded setup to install.</dt>".
-                   "<dt>SUSE ".
-                   "Stable: ".
-                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SLED_10/\">SLED10</a>,".
-                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SUSE_Linux_10.1/\">10.1</a>,".
-                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/openSUSE_10.2/\">10.2</a>".
-                   "Unstable: ".
-                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SLED_10/\">SLED10</a>,".
-                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SUSE_Linux_10.1/\">10.1</a>,".
-                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.2/\">10.2</a>".
-                   "<br>add a repository with zypper eg.".
-"<pre>zypper sa http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.2".
-"zypper --non-interactive in OpenOffice_org-gnome</pre>".
-                    "</dt>".
-                    "<dt>Debian</dt>".
+                   "<dt><a href=\"http://go-oo.org/tstnvl/GoOo/\">Windows</a></dt>\n".
+		   "<dd>Simply download the zip file and run the embedded setup to install.</dd>".
+                   "<dt>SUSE</dt>".
+                   "<dd>Stable: ".
+		   "<ul>".
+                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SLED_10/\">SLED10</a></li>".
+                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SUSE_Linux_10.1/\">10.1</a></li>".
+                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/openSUSE_10.2/\">10.2</a></li>".
+		   "</ul></dd>".
+                   "<dd>Unstable: ".
+		   "<ul>" .
+                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SLED_10/\">SLED10</a></li>".
+                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SUSE_Linux_10.1/\">10.1</a></li>".
+                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.2/\">10.2</a></li>".
+		   "</ul>".
+                   "To add a repository with <a href=\"http://en.opensuse.org/Zypper\">zypper</a>:".
+"<pre># zypper sa http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.2\n".
+"# zypper --non-interactive in OpenOffice_org-gnome</pre>".
+                    "</dd>".
+                    "<dt>Debian</dt>\n".
+		    "<dd>Debian provides OpenOffice.org in their repositories. ".
+		    "To install it just type as root <pre># apt-get install openoffice.org</pre></dd>\n".
                     "<dt>Ubuntu</dt>".
-                    "<dt>Mandriva</dt>".
+		    "<dd>Ubuntu provides OpenOffice.org in their repositories. ".
+		    "To install it just type as root <pre># apt-get install openoffice.org</pre></dd>\n".
+                    "<dt>Mandriva</dt>\n".
+		    "<dd></dd>\n".
                     "<dt>Gentoo</dt>".
+		    "<dd></dd>\n".
                     "<dt>Ark</dt>".
+		    "<dd></dd>\n".
                     "<dt>PLD</dt>".
+		    "<dd></dd>\n".
                     "<dt>RPath</dt>".
+		    "<dd></dd>\n".
                     "<dt>Pardus</dt>".
+		    "<dd></dd>\n".
                     "<dt>QiLinux</dt>".
+		    "<dd></dd>\n".
                     "<dt>DroplineGNOME</dt>".
+		    "<dd></dd>\n".
                     "<dt>Frugalware</dt>".
+		    "<dd></dd>\n".
                     "<dt>Solaris</dt>".
+		    "<dd></dd>\n".
                     "</dl>"
 		    
 );
