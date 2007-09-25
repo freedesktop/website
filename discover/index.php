9a736d9111f04d281e41793e21d3a298b811aec7
Michael Meeks <michael.meeks@novell.com>
2007-09-25 08:29:58 +0000

    Add types.

--- discover/index.php
+++ discover/index.php
@@ -8,23 +8,23 @@ print_page("Go-OO! - Discover", "discover",
    "regular build of OpenOffice.org</p>\n" .
 
    "<table>".
-   "<tr>".
+   "<tr>\n".
 
    "<td>".
    "<h4>VBA support</h4>".
    "<p>Go-oo provide VBA macros support for OpenOffice.org <!-- (<a href=\"excel-vba.xls\">download</a>) --></p>".
    "<p><a href=\"go-excel-vba.png\">".
    "<img src=\"go-excel-vba-thumb.png\"/></a></p>".
-   "</td>".
+   "</td>\n".
 
    "<td>".
    "<h4>Startup performance</h4>".
    "<p>Go-oo starts faster: (go-oo 2.1 vs. Sun/OO.o 2.3)</p>".
    "<img src=\"startup.png\"/></a>".
-   "</td>".
+   "</td>\n".
 
    "</tr>".
-   "<tr>".
+   "<tr>\n".
 
    "<td colspan=\"2\">".
    "<h4>GStreamer integration</h4>".
@@ -38,33 +38,33 @@ print_page("Go-OO! - Discover", "discover",
    "<td><a href=\"go-gstreamer.png\"><img src=\"go-gstreamer-thumb.png\"".
    "/></a></td></tr>\n".
    "</table>".
-   "</td>".
+   "</td>\n".
 
    "</tr>".
-   "<tr>".
+   "<tr>\n".
 
    "<td>".
    "<h4>MS-Works import</h4>".
-   "<p>Go-oo supports MS-Works files (<a href=\"msworks.wps\">download</a>)</p>".
+   "<p>Go-oo supports MS-Works files (<a href=\"msworks.wps\" type=\"application/vnd.ms-works\">download</a>)</p>".
    "<a href=\"go-msworks.png\"><img src=\"go-msworks-thumb.png\" /></a>".
-   "</td>".
+   "</td>\n".
 
    "<td>".
    "<h4>WordPerfect Graphics import</h4>".
    "<p>Go-oo imports graphics in the WPG format coming from WordPerfect".
    "which supplement the WordPerfect importer also available in Go-oo ".
-   "(<a href=\"wpg-support.wpg\">download</a>)</p>".
+   "(<a href=\"wpg-support.wpg\" type=\"image/wpg\">download</a>)</p>".
    "<a href=\"go-wpg-support.png\"><img src=\"go-wpg-support-thumb.png\" /></a>".
-   "</td>".
+   "</td>\n".
 
    "</tr>".
-   "<tr>".
+   "<tr>\n".
 
    "<td>".
    "<h4>T602 import</h4>".
    "<p>Go-oo supports T602 files (<a href=\"cti_lasr.602\">download</a>)</p>".
    "<a href=\"go-t602.png\"><img src=\"go-t602-thumb.png\" /></a>".
-   "</td>".
+   "</td>\n".
 
    "<td>".
    "<h4>Improved EMF rendering</h4>".
@@ -77,10 +77,10 @@ print_page("Go-OO! - Discover", "discover",
    "<td><a href=\"go-emf.png\"><img src=\"go-emf-thumb.png\"".
    "/></a></td></tr>\n".
    "</table>".
-   "</td>".
+   "</td>\n".
 
    "</tr>".
-   "</table>".
+   "</table>\n".
 
 //   "<h4>Office OpenXML import</h4>".
 //   "<p>Go-oo integrates the work in progress made with importing documents in the Microsoft ".
