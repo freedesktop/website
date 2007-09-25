27c5d1bc7d5fe66d5946f7420306f8c15e1fa43f
Hubert Figuiere <hub@figuiere.net>
2007-09-25 16:03:42 +0000

    Added solver fixed a few typos

--- discover/index.php
+++ discover/index.php
@@ -12,7 +12,7 @@ print_page("Go-OO! - Discover", "discover",
 
    "<td>".
    "<h4>VBA support</h4>".
-   "<p>Go-oo provide VBA macros support for OpenOffice.org <!-- (<a href=\"excel-vba.xls\">download</a>) --></p>".
+   "<p>Go-oo provide VBA macros support for OpenOffice.org <!-- (<a href=\"excel-vba.xls\">download sample file</a>) --></p>".
    "<p><a href=\"go-excel-vba.png\">".
    "<img src=\"go-excel-vba-thumb.png\"/></a></p>".
    "</td>\n".
@@ -23,7 +23,16 @@ print_page("Go-OO! - Discover", "discover",
    "<img src=\"startup.png\"/></a>".
    "</td>\n".
 
-   "</tr>".
+   "</tr>\n".
+
+   "<tr><td>\n".
+   "<h4>Calc solver</h4>\n".
+   "<p>Go-oo has a linear optimization solver to optimize a cell value based\n".
+   "on arbitrary constraints, built into Calc.</p>".
+   "<a href=\"go-calc-solver.png\">".
+   "<img src=\"go-calc-solver-thumb.png\" /></a>".
+   "</td></tr>\n".
+
    "<tr>\n".
 
    "<td colspan=\"2\">".
@@ -45,15 +54,15 @@ print_page("Go-OO! - Discover", "discover",
 
    "<td>".
    "<h4>MS-Works import</h4>".
-   "<p>Go-oo supports MS-Works files (<a href=\"msworks.wps\" type=\"application/vnd.ms-works\">download</a>)</p>".
+   "<p>Go-oo supports MS-Works files (<a href=\"msworks.wps\" type=\"application/vnd.ms-works\">download sample file</a>)</p>".
    "<a href=\"go-msworks.png\"><img src=\"go-msworks-thumb.png\" /></a>".
    "</td>\n".
 
    "<td>".
    "<h4>WordPerfect Graphics import</h4>".
-   "<p>Go-oo imports graphics in the WPG format coming from WordPerfect".
+   "<p>Go-oo imports graphics in the WPG format coming from WordPerfect ".
    "which supplement the WordPerfect importer also available in Go-oo ".
-   "(<a href=\"wpg-support.wpg\" type=\"image/wpg\">download</a>)</p>".
+   "(<a href=\"wpg-support.wpg\" type=\"image/wpg\">download sample file</a>)</p>".
    "<a href=\"go-wpg-support.png\"><img src=\"go-wpg-support-thumb.png\" /></a>".
    "</td>\n".
 
@@ -62,7 +71,7 @@ print_page("Go-OO! - Discover", "discover",
 
    "<td>".
    "<h4>T602 import</h4>".
-   "<p>Go-oo supports T602 files (<a href=\"cti_lasr.602\" type=\"application/x-t602\">download</a>)</p>".
+   "<p>Go-oo supports T602 files (<a href=\"cti_lasr.602\" type=\"application/x-t602\">download sample file</a>)</p>".
    "<a href=\"go-t602.png\"><img src=\"go-t602-thumb.png\" /></a>".
    "</td>\n".
 
@@ -80,6 +89,7 @@ print_page("Go-OO! - Discover", "discover",
    "</td>\n".
 
    "</tr>".
+
    "</table>\n".
 
 //   "<h4>Office OpenXML import</h4>".
