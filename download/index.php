4744f79ffdfb86f1eaf61d0685189522a361b0e2
Michael Meeks <michael.meeks@novell.com>
2007-09-25 09:47:01 +0000

    Shrink the suse section ...

--- download/index.php
+++ download/index.php
@@ -9,18 +9,16 @@ print_page("Go-OO! - Download", "download",
 		   "<dd>Simply download the zip file and run the embedded setup to install.</dd>".
                    "<dt>SUSE</dt>".
                    "<dd>Stable: ".
-		   "<ul>".
-                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SLED_10/\">SLED10</a></li>".
-                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SUSE_Linux_10.1/\">10.1</a></li>".
-                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.2/\">10.2</a></li>".
-		   "</ul></dd>".
+                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SLED_10/\">SLED10</a>, ".
+                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SUSE_Linux_10.1/\">10.1</a>, ".
+                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.2/\">10.2</a>".
+		   "</dd>".
                    "<dd>Unstable: ".
-		   "<ul>" .
-                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SLED_10/\">SLED10</a></li>".
-                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SUSE_Linux_10.1/\">10.1</a></li>".
-                   "<li><a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/openSUSE_10.2/\">10.2</a></li>".
-		   "</ul>".
-                   "To add a repository with <a href=\"http://en.opensuse.org/Zypper\">zypper</a>:".
+                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SLED_10/\">SLED10</a>, ".
+                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SUSE_Linux_10.1/\">10.1</a>, ".
+                   "<a href=\"http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/openSUSE_10.2/\">10.2</a>".
+		   "</dd>".
+                   "<dd>To add a repository with <a href=\"http://en.opensuse.org/Zypper\">zypper</a>:".
 "<pre># zypper sa http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.2\n".
 "# zypper --non-interactive in OpenOffice_org-gnome</pre>".
                     "</dd>".
