214a40a5d91eb5c2ae2f4a86608f30c4c86849df
Michael Meeks <michael.meeks@novell.com>
2008-05-02 12:52:44 +0000

    Add one-click download ...

--- download/index.php
+++ download/index.php
@@ -37,18 +37,14 @@ To check out:
 <h2>Other derivatives</h2>
 
 <h3>openSUSE</h3>
-<dd>
+<p><a href="http://en.opensuse.org/OpenOffice.org">Information</a>, and Repositories:
 <a href="http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SLED_10/">SLED10</a>, 
 <a href="http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/SUSE_Linux_10.1/">10.1</a>, 
-<a href="http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.2/">10.2</a>
-<a href="http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.3/">10.3</a> (<a href="http://software.opensuse.org/ymp/OpenOffice.org:STABLE/openSUSE_10.3/OpenSUSE_org.ymp">1-click install</a>)
-</dd>
-<dd>To add a repository with <a href="http://en.opensuse.org/Zypper">zypper</a> (NB. OO.o
-has been split into more individual packages here so make sure you get all of them):
-<pre># zypper sa http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/openSUSE_10.2
-# zypper --non-interactive in OpenOffice_org-gnome OpenOffice_org-writer OpenOffice_org-calc ...</pre>
-</dd>
-<dd>See <a href="http://en.opensuse.org/OpenOffice.org">opensuse.org</a> site for more information about these builds.</dd>
+<a href="http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.2/">10.2</a>,
+<a href="http://download.opensuse.org/repositories/OpenOffice.org:/STABLE/openSUSE_10.3/">10.3</a>
+<a href="http://software.opensuse.org/ymp/OpenOffice.org:STABLE/openSUSE_10.3/OpenSUSE_org.ymp">
+<img src="opensuse-1click.png" title="1-click install" alt="1-click install" /></a></p>
+<br>
 <dl>
 
 <h3>Debian</h3>
@@ -98,16 +94,25 @@ type as root:
 
 <h2>Unstable / development packages</h2>
 
- <a href="http://go-oo.mirrorbrain.org/unstable/win32/">UNSTABLE</a> build.</dd>
+<dd>Prior to new releases, we provide builds of the next Go-OO!
+on the web, but these are unstable, may be older, may break: in which
+case you get to keep both bits.</dd>
 
- <a href="http://go-oo.mirrorbrain.org/unstable/linux-x86/">UNSTABLE</a> build.</dd>
-
-SUSE:
-<dd>UNSTABLE: 
+<dd><a href="http://go-oo.mirrorbrain.org/unstable/win32/">Windows</a>,
+<a href="http://go-oo.mirrorbrain.org/unstable/linux-x86/">Universal Linux</a></dd>
+<dd>SUSE:
 <a href="http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SLED_10/">SLED10</a>, 
 <a href="http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/SUSE_Linux_10.1/">10.1</a>, 
 <a href="http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/openSUSE_10.2/">10.2</a>
-<a href="http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/openSUSE_10.3/">10.3</a> (<a href="http://software.opensuse.org/ymp/OpenOffice.org:UNSTABLE/openSUSE_10.3/OpenSUSE_org.ymp">1-click install</a>)
+<a href="http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/openSUSE_10.3/">10.3</a>
+<a href="http://software.opensuse.org/ymp/OpenOffice.org:UNSTABLE/openSUSE_10.3/OpenSUSE_org.ymp">
+<img src="opensuse-1click.png" title="1-click install" alt="1-click install" width="101"
+align="baseline" border="0" height="26"/></a>
+</dd>
+<dd>To add a repository with <a href="http://en.opensuse.org/Zypper">zypper</a> (NB. OO.o
+has been split into more individual packages here so make sure you get all of them):
+<pre># zypper sa http://download.opensuse.org/repositories/OpenOffice.org:/UNSTABLE/openSUSE_10.2
+# zypper --non-interactive in OpenOffice_org-gnome OpenOffice_org-writer OpenOffice_org-calc ...</pre>
 </dd>
 
 EOT;
