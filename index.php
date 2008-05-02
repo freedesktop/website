501a9758087f8c59f287eb03a77e06b62a7844dd
Michael Meeks <michael.meeks@novell.com>
2008-05-02 13:05:11 +0000

    Update front-page, fix typos etc.

--- index.php
+++ index.php
@@ -8,10 +8,11 @@ $content = <<<EOT
     <h3>Better interoperability</h3>
     <p>
 	Go-oo has built in <a href="/discover/#docx-support">OpenXML</a>
-	import filters, it will import your
+	import filters and it will import your
 	Microsoft <a href="/discover/#ms-works-import">Works</a> files.
-	Compared to up-stream OO.o, it has
-	better Microsoft binary file support (with eg. fields
+	Compared with up-stream OO.o, it has
+	better Microsoft binary file support (with eg. <a
+	href="/discover/#fields-support">fields</a>
 	support), and it will import 
 	<a href="/discover/#wp-graphics-import">WordPerfect</a>
 	graphics beautifully. If you are reliant on Excel
@@ -29,7 +30,7 @@ $content = <<<EOT
 	multimedia</a> integration on Linux, a beautiful <a
 	href="/discover/#calc-solver">solver</a> component, and your <a
 	href="/discover/#chinese-rendering">Chinese</a> should look sane.
-	We also integrate with your system better by default, enabling
+	We also integrate with your system better by default: eg. enabling
 	native file-selectors on Linux.
     </p>
 
@@ -37,7 +38,8 @@ $content = <<<EOT
     <p>
 	From first-time startup, where we sort I/O to reduce seek cost,
 	to a highly optimised second start application and a systray <a
-	href="/discover/#quickstarter">quick-starter</a> on Linux.
+	href="/discover/#quickstarter">quick-starter</a> on Linux we
+	are faster.
 	We use less memory than up-stream, we link faster, use better
 	system allocators, and don't waste so much time &amp; memory in
 	the registry. Go-oo performance is hard to beat.
@@ -48,15 +50,15 @@ $content = <<<EOT
 	Contributing code to go-oo is simple, and fast, following the
 	traditional hackers' process of peer code review: just mail
 	patches to the <a href="http://lists.go-oo.org/listinfo.cgi/dev-go-oo.org">
-	list</a>, or if we like your code - commit your patch
-	immediately to HEAD ooo-build: no CWS, no hours of tagging,
-	paperwork, no specification, no beaurocracy. Of course - if
-	your patch sucks - be prepared to hear about it.
+	mailing list</a>, or when we get used to your code - commit your
+	patch immediately to HEAD ooo-build: no CWS, no hours of tagging,
+	paperwork, no specification, no hassle. Of course - if
+	your patch sucks - expect to hear how it can be improved.
     </p>
 
     <h3>Freer licensing</h3>
     <p>
-	For the code to live, grow and improve, to encourage participation, and
+	For the code to live, grow and improve, to encourage participation and
 	compete with the other office suite - we need sensible licensing: ie.
 	weak copy-left. While in general we think LGPLv3 is a great &amp; sufficient
 	license for our code, others eg. <a
@@ -65,7 +67,7 @@ $content = <<<EOT
 	href="http://council.openoffice.org/files/documents/126/4135/OOoAdvisoryBoard_01Nov2007_MeetingNotes_v4.pdf">
 	IBM</a> appear reluctant to include LGPL code into their products, and prefer
 	other licenses such as the CDDL (a weak copy-left derived from Mozilla's MPL).
-	Luckily dual licensing under the LGPLv3 / CDDL can help - and we recommend
+	Luckily dual licensing under the LGPLv3 / CDDL can help here - and we recommend
 	this for the majority of our code.
     </p>
     <p>
