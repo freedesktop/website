dd18bec273c8f9fda9a9432d54d4af5314db8d97
Jan Holesovsky <kendy@suse.cz>
2010-08-06 21:32:01 +0200

    Nicer logging.

--- cron.sh
+++ cron.sh
@@ -4,6 +4,10 @@ cd /srv/planet-config
 
 (
     flock 200
-    date >> /var/log/planet.log
-    python /srv/planet-2.0/planet.py config.ini >> /var/log/planet.log 2>&1
+    (
+        echo "***********************************************************************"
+        echo -n "Date: "
+        date
+        python /srv/planet-2.0/planet.py config.ini
+    ) >> /var/log/planet.log 2>&1
 ) 200>/srv/planet-config/planet.lock
