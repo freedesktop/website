0921de8ce3e3addd74a9a752a5201b84d603a7d0
Jan Holesovsky <kendy@suse.cz>
2010-08-13 11:09:35 +0200

    Update the log location.

--- cron.sh
+++ cron.sh
@@ -9,5 +9,5 @@ cd /srv/planet-config
         echo -n "Date: "
         date
         python /srv/planet-2.0/planet.py config.ini
-    ) >> /var/log/planet.log 2>&1
+    ) >> /var/log/planet/planet.log 2>&1
 ) 200>/srv/planet-config/planet.lock
