e128cc6e5352d772698e381f96f2640052fc454f
Jan Holesovsky <kendy@suse.cz>
2010-08-19 13:05:41 +0200

    Remove the flock, tinderbox has its own locking mechanism.

--- bin/tinderupdate.sh
+++ bin/tinderupdate.sh
@@ -6,9 +6,6 @@ TINDERBOX_LOG=/var/log/tinderbox/tinderbox2.log
 
 cd /srv/tinderbox
 
-(
-flock 200
-
 echo starting update at   $(date -u) >> $TINDERBOX_LOG
 # update the tag-lists
 touch $TINDERBOX_WWW/tags/tag-list.tstamp
@@ -21,5 +18,3 @@ $TINDERBOX_BIN/bypass_postfixlimit.sh >> $TINDERBOX_LOG 2>&1
 # regenerate tinderbox-HTML pages
 $TINDERBOX_WWW/tinder.cgi >> $TINDERBOX_LOG 2>&1
 echo finished updating at $(date -u) >> $TINDERBOX_LOG
-
-) 200>$TINDERBOX_BIN/tinderbox.lock
