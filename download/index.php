89a07ffe51dbe0c2313500cea87455d37732b449
Jan Holesovsky <kendy@suse.cz>
2010-10-13 21:44:41 +0200

    Beta2 is available for download.

--- download/index.php
+++ download/index.php
@@ -3,7 +3,7 @@ require("../template.php");
 
 $urlbase=$download_binaries;
 
-$content = <<<"EOT"
+$content = <<<EOT
 
 
 <div id="libreoffice">
@@ -19,7 +19,7 @@ office suites, and available on a variety of platforms. It is free software
 and therefore free to download, use and distribute.
 </p>
 
-<h3>Download LibreOffice</h3>
+<h3>Download LibreOffice Beta2</h3>
 
 <p>
 The following software packages are intended to give you a first impression of what LibreOffice 
@@ -27,7 +27,7 @@ is. Currently we are in the process of combining the work of many contributors, 
 how to make the software available to our users. 
 There are known issues, so please consider reading the <a href="#release-notes">release notes</a>
 below for a list of known issues being worked on, the most obvious being
-the missing language packaging situation.
+the incomplete language packaging situation.
 </p>
 
 <p>
@@ -36,32 +36,36 @@ This beta release is not intended for production use! <b>Be advised that the cur
 
 <div id="download-nav">
     <ul id="dlnav">
-       <li class="dlnav-win32"><a href="$urlbase/LO_3.3.0-beta1_Win_x86_install_en-US.exe">Download and install this for the English version to be used on Microsoft Windows.</a></li>
-       <li class="dlnav-linux32"><a href="$urlbase/LO_3.3.0-beta1_Linux_x86_install-rpm_en-US.tar.gz">Download and install this for the English version to be used on various GNU/Linux 32-bit distributions.</a></li>
-       <li class="dlnav-linux64"><a href="$urlbase/LO_3.3.0-beta1_Linux_x86-64_install-rpm_en-US.tar.gz">Download and install this for the English version to be used on various GNU/Linux 64-bit distributions.</a></li>
-       <li class="dlnav-mac"><a href="$urlbase/LO_3.3.0-beta1_MacOS_x86_install_en-US.dmg">Download and install this for the English version to be used on Apple MacOS X.</a></li>
+       <li class="dlnav-win32"><a href="$urlbase/3.3.0-beta2/win/x86/LibO_3.3.0_beta2_Win_x86_install_multi.exe">Download and install this for the International version to be used on Microsoft Windows.</a></li>
+       <li class="dlnav-linux32"><a href="$urlbase/3.3.0-beta2/rpm/x86/LibO_3.3.0_beta2_Linux_x86_install-rpm_en-US.tar.gz">Download and install this for the English version to be used on various GNU/Linux 32-bit distributions.</a></li>
+       <li class="dlnav-linux64"><a href="$urlbase/3.3.0-beta2/rpm/x86_64/LibO_3.3.0_beta2_Linux_x86-64_install-rpm_en-US.tar.gz">Download and install this for the English version to be used on various GNU/Linux 64-bit distributions.</a></li>
+       <li class="dlnav-mac"><a href="$urlbase/3.3.0-beta2/mac/x86/LibO_3.3.0_MacOS_x86_install_en-US.dmg">Download and install this for the English version to be used on Apple MacOS X.</a></li>
        <li class="dlnav-source"><a href="$download_source">Click here to download the tarball containing the source code.</a></li>
     </ul>
 
-    <p>You can also <a href="http://tracker.documentfoundation.org:6969">download using BitTorrent</a>.</p>
+    <p>You can also <a href="http://tracker.documentfoundation.org:6969">download using BitTorrent</a>,
+    or <a href="$urlbase/3.3.0-beta2">browse all the Beta2 installation packages</a> to get inofficial
+    Debian packages, or language packs.</p>
 </div>
 
 <h3 id="release-notes">Beta Release Notes</h3>
 <p>This beta release is not intended for production use!</p>
 <p>There are a number of known issues being worked on:</p>
 <ul class="list-libreoffice">
-<li>All builds are English-only (en-US). We regret that we were unable to provide other 
-    localized versions. We are currently working on an improved mechanism to make 
-    localized versions in your language available.</li>
+<li>The Windows build is an International build - you can choose the user 
+    interface language that is suitable for you, but the help is always English.
+    We are currently working on improving the delivery mechanism to be able to 
+    provide you with the localized help.
+    We are also working on smaller problems like
+    <a href="https://bugs.freedesktop.org/show_bug.cgi?id=30797">wrong description
+    of several languages</a>.</li>
+<li>The Linux and MacOSX builds are English builds with the possibility to install language packs.
+    Please browse <a href="$urlbase/3.3.0-beta2">the archives</a> to get the langugage pack you need 
+    for your platform.</li>
 <li>The LibreOffice branding and renaming is new and work in progress. You may still see old graphics, 
     icons or websites. So please bear with us. This also applies to
-	the BrOffice.org branding - applicable in Brazil.</li>
-<li>If you want to extend LibreOffice with new features via Extensions, then the link within the 
-    software refers, in error, to the OpenOffice.org Extension website. For Free Software extensions, please use
-    the FSF Extension List at <a
-	href="http://fsf.org/openoffice">http://fsf.org/openoffice</a>
-	instead.</li>
-<li>The current LibreOffice beta replaces any existing OpenOffice.org installation, on Windows. Installations should co-exist in the future.</li>
+    the BrOffice.org branding - applicable in Brazil.</li>
+<li>Filters for the legacy StarOffice binary formats are missing.</li>
 </ul>
 
 <h3>Keeping Up With the Progress</h3>
