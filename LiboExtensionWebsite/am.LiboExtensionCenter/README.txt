fe63b2564b4d43e0a06ba0719ada8eadc6418c1a
Andreas Mantke <maand@gmx.de>
2011-04-21 23:34:35 +0200

    Initial commit

---
+++ LiboExtensionWebsite/am.LiboExtensionCenter/README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+This package creates a protension website for LibreOffice that provide users direct access to such add-ons.
+
+This product may contain traces of nuts.
---
+++ LiboExtensionWebsite/am.LiboExtensionCenter/am/LiboExtensionCenter/tests.py
@@ -0,0 +1,55 @@
+import unittest
+
+#from zope.testing import doctestunit
+#from zope.component import testing
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import fiveconfigure
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+ptc.setupPloneSite()
+
+import am.LiboExtensionCenter
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            fiveconfigure.debug_mode = True
+            ztc.installPackage(am.LiboExtensionCenter)
+            fiveconfigure.debug_mode = False
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='am.LiboExtensionCenter',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='am.LiboExtensionCenter.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        #ztc.ZopeDocFileSuite(
+        #    'README.txt', package='am.LiboExtensionCenter',
+        #    test_class=TestCase),
+
+        #ztc.FunctionalDocFileSuite(
+        #    'browser.txt', package='am.LiboExtensionCenter',
+        #    test_class=TestCase),
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
