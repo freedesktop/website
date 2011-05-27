9317994a48c9b9a3600cd2a74c4087a03b793aa7
Andreas Mantke <maand@gmx.de>
2011-05-27 17:53:07 +0200

    reworked base.py

--- LiboExtensionWebsite/am.LiboExtensionCenter/am/LiboExtensionCenter/tests/base.py
+++ LiboExtensionWebsite/am.LiboExtensionCenter/am/LiboExtensionCenter/tests/base.py
@@ -28,7 +28,7 @@ from Products.PloneTestCase.layer import PloneSite
 
 @onsetup
 def install_plugins():
-    ZopeTestCase.installPackage('collective.psc.externalstorage')
+    ZopeTestCase.installPackage('collective.lec.externalstorage')
 
 install_plugins()
 setupPloneSite(products=['am.LiboExtensionCenter',])
@@ -54,17 +54,17 @@ class LECTestCase(PloneTestCase):
     
             # loading externalstorage if present in the environment
             try:
-                import collective.psc.externalstorage
+                import collective.lec.externalstorage
             except ImportError:
                 pass
             else:
-                ext = os.path.dirname(collective.psc.externalstorage.__file__)
+                ext = os.path.dirname(collective.lec.externalstorage.__file__)
                 config = os.path.join(ext, 'configure.zcml')
                 zcml.load_config(config,
-                                 collective.psc.externalstorage)
+                                 collective.lec.externalstorage)
                 from zope.component import getGlobalSiteManager
-                from collective.psc.externalstorage.config import ESConfiguration
-                from collective.psc.externalstorage.interfaces import IESConfiguration
+                from collective.lec.externalstorage.config import ESConfiguration
+                from collective.lec.externalstorage.interfaces import IESConfiguration
                 gsm = getGlobalSiteManager()
                 gsm.registerUtility(ESConfiguration, IESConfiguration)
 
