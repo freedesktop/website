cd83edd876770e0c98c8bd5644248a9c83ad27db
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2011-12-14 16:19:13 +0100

    no need for getting tag & masterlists from no longer existing infrastructure

--- bin/tinderupdate.sh
+++ bin/tinderupdate.sh
@@ -11,8 +11,6 @@ cd /srv/tinderbox
 echo starting update at   $(date -u) >> $TINDERBOX_LOG
 # update the tag-lists
 touch $TINDERBOX_WWW/tags/tag-list.tstamp
-$TINDERBOX_BIN/get-taglists.pl
-$TINDERBOX_BIN/get-latest-master.pl
 
 # parse mails with attachment that accumulated in the meantime
 $TINDERBOX_BIN/bypass_postfixlimit.sh >> $TINDERBOX_LOG 2>&1
